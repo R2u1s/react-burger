d3409cb54dbb7e4a7aa31a4e20c093d63bd6a0be
Ruslan Budanov <Budz@yandex.ru>
2023-04-03 10:56:30 +0300

    одно модальное окно на ингредиенты

--- src/components/BurgerIngredients/BurgerIngredients.js
+++ src/components/BurgerIngredients/BurgerIngredients.js
@@ -19,16 +19,42 @@ const data = {
 }
 
 function BurgerIngredients(){
+
+  const [modalActive, setModalActive] = React.useState(false);
+  const [modalValues, setIngredientValues] = React.useState({
+    image: '',
+    name: '',
+    calories: '',
+    proteins: '',
+    fat: '',
+    carbohydrates: ''
+  });
+
+  function openIngredientPreview(e) {
+    const ingredientDOM = e.target.closest('[name="ingredient"]');
+    if (ingredientDOM) {
+      setIngredientValues(initialData.find((item) => {
+        return item._id === ingredientDOM.id;
+      }));
+      setModalActive(true);
+    }
+  }
+   
   return (
-    <section className={styles['burger-ingredients']}>
-      <h1 className={`mb-5 text text_type_main-large`}>Соберите бургер</h1>
-      <Tabs />
-      <ul className={styles['burger-ingredients__ingredients']}>
-        <BurgerIngredientsGroup ingredient={{type:"Булки", list: data.buns}} />
-        <BurgerIngredientsGroup ingredient={{type:"Соусы", list: data.sauces}}/>
-        <BurgerIngredientsGroup ingredient={{type:"Начинка", list: data.mains}}/>
-      </ul>
-    </section>
+    <>
+      <section className={styles['burger-ingredients']}>
+        <h1 className={`mb-5 text text_type_main-large`}>Соберите бургер</h1>
+        <Tabs />
+        <ul className={styles['burger-ingredients__ingredients']} onClick={openIngredientPreview}>
+          <BurgerIngredientsGroup ingredient={{type:"Булки", list: data.buns}} />
+          <BurgerIngredientsGroup ingredient={{type:"Соусы", list: data.sauces}}/>
+          <BurgerIngredientsGroup ingredient={{type:"Начинка", list: data.mains}}/>
+        </ul>
+      </section>
+      <Modal active={modalActive} setActive={setModalActive}>
+        <IngredientDetails ingredientDetails={modalValues} />
+      </Modal>
+    </>
   );
 }
 
